3ee9a9210d08f78af1aa302b5bfafb6f3669031c
thegypsyboyo <lewisonyango9@gmail.com>
2024-01-30 16:13:02 +0300

    Footer component with accepted payments, and copyrights section

---
+++ components/footer/NewsLetter.js
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import { useState } from "react";
+import styles from "./styles.module.scss";
+import axios from "axios";
+export default function NewsLetter() {
+ 
+  return (
+    <div className={styles.footer__newsletter}>
+      <h3>SIGN UP FOR OUR NEWSLETTER</h3>
+      <div className={styles.footer__flex}>
+        <input
+          type="text"
+          placeholder="Your Email Address"
+        />
+        <button
+          className={styles.btn_primary}
+          style={{  cursor: "pointer"}}
+        >
+          SUBSCRIBE
+        </button>
+      </div>
+      
+      <p>
+        By clicking the SUBSCRIBE button, you are agreeing to{" "}
+        <Link href="">our Privacy & Cookie Policy</Link>
+      </p>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Header from "../components/header"
-
+import Footer from "../components/footer"
 
 
 export default function Home() {
   return (
     <div className="">
       <Header />
+      <Footer />
     </div>
   );
 }
