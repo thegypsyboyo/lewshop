07c7289e66a6b6b137b7733911ef91383c676746
thegypsyboyo <lewisonyango9@gmail.com>
2024-02-06 19:32:24 +0300

    we have added github and google providers to the app

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import { useSession, signIn, signOut } from "next-auth/react"
 export default function Home({ country}) {
 
   const { data: session } = useSession() 
-
+  console.log("Data:", session)
   return (
     <div className="">
       <Header country={country} />
