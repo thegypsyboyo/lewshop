346f8ae7c656b757c59e208eac048bdfbf861eb0
thegypsyboyo <lewisonyango9@gmail.com>
2024-02-07 20:26:50 +0300

    We have created a model and can communicate with our DB, we can also configured a activation token using jwt

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -3,7 +3,12 @@
 import nc from 'next-connect'
 import db from "../../../utils/db"
 
+import bcrypt from "bcrypt"
+
 import { validateEmail } from '../../../utils/validation';
+
+import User from "../../../models/User"
+import { createActivationToken } from '../../../utils/token';
 
 const handler = nc();
 
@@ -18,10 +23,30 @@ handler.post(async (req,res)=> {
         if (!validateEmail(email)) {
             return res.status(400).json({ message: "Invalid email."})
         }
+
+        const user  = await User.findOne({email})
+        if (user){
+            return res.status(400).json({ message: "This email already exists"})
+        }
+
+        if (password.length < 6) {
+            return res.status(400).json({ message: "Password must be atleast 6 characters"})
+        }
+
+        const cryptedPassword = await bcrypt.hash(password, 12);
+
+        const newUser = new User({ name, email, password:cryptedPassword})
+        const addedUser = await newUser.save();
+
+        const activation_token = createActivationToken({
+            id: addedUser._id.toString(),
+        });
+        
         console.log(req.body);
+        res.send(activation_token);
         
     } catch (error) {
-        req.status(500).json({message:error.message})
+        res.status(500).json({message:error.message})
     }
 });
 
